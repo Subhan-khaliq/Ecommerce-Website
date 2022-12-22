3191331f0a667ee630fdc4c04d08ebcea79ebfeb
Subhan Khaliq <subhankhaliq420@gmail.com>
2022-12-22 13:32:19 +0500

    Test

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -9,7 +9,7 @@
 
 set :rails_env, 'staging'
 set :puma_env, fetch(:rack_env, fetch(:rails_env, 'staging'))
-server '54.90.89.158', user: 'deploy', roles: %w{web app db}
+server '54.163.60.223', user: 'deploy', roles: %w{web app db}
 
 # role-based syntax
 # ==================
